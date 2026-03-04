a952e5d6e292c04fd304c71953b03fe8debf3511
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-03-04 19:20:05 +0000

    cambios de implementacion y en curar

--- src/daw/app/model/magico/Mago.java
+++ src/daw/app/model/magico/Mago.java
@@ -1,43 +1,36 @@
 package model.magico;
-
 import model.Personaje;
-import interfaces.Curable;
-import interfaces.Estratega;
+import model.interfaces.Curable;
 
-public class Mago extends Personaje implements Curable, Estratega {
-    private int elemento;
+public class Mago extends PersonajeMagico implements Curable {
+    private String elemento;
 
-    public Mago(String id, String nombre, int nivel, int puntosDeVida, int mana, int inteligencia, String elemento){
-        super(id, nombre, nivel, puntosDeVida, mana, inteligencia);
-        this.elemento = asignarElemento(elemento);
+    public Mago(String nombre, int nivel, int puntosDeVida, int mana, int inteligencia, String elemento) {
+        super(nombre, nivel, puntosDeVida, mana, inteligencia);
+        this.elemento = elemento;
     }
 
     @Override
-    public void atacar(Personaje objetivo){
-        if (getMana() >= 15){
+    public void atacar(Personaje objetivo) {
+        if (getMana() >= 15) {
             int dano = getInteligencia() * 2;
             objetivo.setPuntosDeVida(objetivo.getPuntosDeVida() - dano);
             setMana(getMana() - 15);
-            System.out.println("Mago"+ getNombre() + " lanza un hechizo de " + getElemento() + " a " + objetivo.getNombre() + " causando "+ dano + " de daño.");
-        } else{
-            System.out.println("El "+ getNombre()+" no tiene suficiente mana para atacar.");
+            System.out.println("El Mago " + getNombre() + " lanza un hechizo de " + elemento + " a " + objetivo.getNombre() + " causando " + dano + " de daño.");
+        } else {
+            System.out.println(getNombre() + " no tiene suficiente maná para atacar. ¡Debe meditar!");
+            meditar();
         }
     }
 
     @Override
-    public void curar(){
-        if cura = getInteligencia() * 3;
-        setPuntosDeVida(getPuntosDeVida() + cura);
-        System.out.println("El "+ getNombre() + "usa su magia para sanarse " + cura + " puntos de salud.");   
-    }
-
-    @Override
-    public void planificar(){
-        System.out.println("El " + getNombre() + " analiza el campo de batalla para optimizar su proximo conjuro.");
+    public void curar(Personaje objetivo) {
+        int cura = getInteligencia() * 2;
+        objetivo.setPuntosDeVida(objetivo.getPuntosDeVida() + cura);
+        System.out.println(getNombre() + " usa su magia elemental para sanar a " + objetivo.getNombre() + " en " + cura + " puntos.");   
     }
     
-    public int getElemento(){
-        return elemento;
+    public String getElemento() { 
+        return elemento; 
     }
-   
 }
