b6fd0697fb3f99d7e30ed0beb5a915129c5a64f5
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-03-04 19:49:13 +0000

    cambios en personajeFisico, debido a las implementaciones automaticas por el IDE del compañero

--- src/daw/app/model/fisico/PersonajeFisico.java
+++ src/daw/app/model/fisico/PersonajeFisico.java
@@ -2,19 +2,13 @@ package model.fisico;
 import model.Personaje;
 
 public abstract class PersonajeFisico extends Personaje {
-    public PersonajeFisico(String nombre, int nivel, int salud){
-        super(nombre, nombre, nivel, salud);
+    
+    public PersonajeFisico(String nombre, int nivel, int puntosDeVida) {
+        super(nombre, nivel, puntosDeVida);
     }
 
-    public void golpeBasico(){
-        System.out.println(getNombre() + " da un golpe básico");
-    }
-
-    public void defender() {
-        throw new UnsupportedOperationException("Unimplemented method 'defender'");
-    }
-
-    public void atacar(Personaje objetivo) {
-        throw new UnsupportedOperationException("Unimplemented method 'atacar'");
+    public void golpeBasico(Personaje objetivo) {
+        System.out.println(getNombre() + " da un golpe básico a " + objetivo.getNombre());
+        objetivo.setPuntosDeVida(objetivo.getPuntosDeVida() - 5);
     }
 }
