1f506f9d0279f25f508739709dcad68a0bfbfd78
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-03-04 19:36:51 +0000

    actualizacion de imports y codigo

--- src/daw/app/controller/JuegoController.java
+++ src/daw/app/controller/JuegoController.java
@@ -1,37 +1,50 @@
 package controller;
-
 import model.Personaje;
-import model.fisico.Defendible;
-import model.fisico.Sigiloso;
+import model.interfaces.Curable;
+import model.interfaces.Defendible;
 import java.util.ArrayList;
 
 public class JuegoController {
-    private ArrayList<Personaje> listaPersonajes;
+    private ArrayList<Personaje> personajes;
 
     public JuegoController() {
-        this.listaPersonajes = new ArrayList<>();
+        this.personajes = new ArrayList<>();
     }
 
     public void agregarPersonaje(Personaje p) {
-        listaPersonajes.add(p);
-    }
-
-    public void simularTurno() {
-        for (Personaje p : listaPersonajes) {
-            p.atacar(p); 
-
-            if (p instanceof Sigiloso) {
-                ((Sigiloso) p).atacar(p);
-            }
-
-            if (p instanceof Defendible) {
-                ((Defendible) p).ocultar();
-                ((Defendible) p).defender();
-            }
-        }
+        personajes.add(p);
     }
 
     public ArrayList<Personaje> getPersonajes() {
-        return listaPersonajes;
+        return personajes;
+    }
+
+    public void simularTurno() {
+        System.out.println("\n---INICIANDO SIMULACIÓN DE TURNO ---");
+        
+        if (personajes.size() < 2) {
+            System.out.println("No hay suficientes personajes para simular un combate.");
+            return;
+        }
+
+        for (int i = 0; i < personajes.size(); i++) {
+            Personaje atacante = personajes.get(i);
+            Personaje objetivo = personajes.get((i + 1) % personajes.size());
+
+            System.out.println("\n Turno de: " + atacante.getNombre());
+            
+            atacante.atacar(objetivo);
+
+            if (atacante instanceof Curable) {
+                System.out.println("   [Capacidad detectada]: Este personaje puede curar.");
+                ((Curable) atacante).curar(atacante); 
+            }
+            
+            if (atacante instanceof Defendible) {
+                System.out.println("   [Capacidad detectada]: Este personaje puede defenderse.");
+                ((Defendible) atacante).defender();
+            }
+        }
+        System.out.println("\n---FIN DEL TURNO---");
     }
 }
