8bdaeb004405d6d8e55fb2e89ea2903649ec690c
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-03-04 19:07:58 +0000

    cambios en clerigo ya que el anterior daba fallos con el implements

--- src/daw/app/model/magico/Clerigo.java
+++ src/daw/app/model/magico/Clerigo.java
@@ -1,32 +1,38 @@
-package daw.app.model.magico;
+package model.magico;
 
-public class Clerigo extends PersonajeMagico implements Curandero{
+import model.Personaje;
+import model.interfaces.Curable;
+
+public class Clerigo extends PersonajeMagico implements Curable {
     private String deidad;
 
-    public Clerigo(String nombre, int nivel, int puntosdevida, int puntosdemana, int fuerza, int inteligencia, String deidad ){
-        super(nombre, nivel, puntosdevida, puntosdemana, fuerza, inteligencia);
+    public Clerigo(String nombre, int nivel, int puntosDeVida, int mana, int inteligencia, String deidad) {
+        super(nombre, nivel, puntosDeVida, mana, inteligencia);
         this.deidad = deidad;
     }
 
     @Override
-    public void atacar(Personaje objetivo){
+    public void atacar(Personaje objetivo) {
         int dano = getInteligencia() * 2;
-        objetivo.setSalud(objetivo.getSalud() - dano);
-        System.out.println("El Clerigo " + getNombre() + " invoca el poder de " + deidad + " y golpea a " + objetivo.getNombre() + " causando " + dano + " de daño sagrado.");
+        objetivo.setPuntosDeVida(objetivo.getPuntosDeVida() - dano);
+        System.out.println("El Clérigo " + getNombre() + " invoca el poder de " + deidad + " y golpea a "
+                + objetivo.getNombre() + " causando " + dano + " de daño sagrado.");
     }
 
     @Override
-    public void curar(Personaje objetivo){
-        if (getMana() >= 20){
+    public void curar(Personaje objetivo) {
+        if (getMana() >= 20) {
             int sanacion = getInteligencia() * 3;
-            setSalud(getSalud() + sanacion);
+            objetivo.setPuntosDeVida(objetivo.getPuntosDeVida() + sanacion);
             setMana(getMana() - 20);
-            System.out.println("CURADO " + getNombre() + " realiza una oración de sanación y recupera " + sanacion + " PS ");
-        } else{
-            System.out.println("NO CURADO " + getNombre() + " No tienes suficiente mana para curar.");
+            System.out.println(getNombre() + " realiza una oración y cura a " + objetivo.getNombre()
+                    + " restaurando " + sanacion + " PS.");
+        } else {
+            System.out.println(getNombre() + " no tiene suficiente maná para curar.");
         }
     }
-    public String getDeidad(){
+
+    public String getDeidad() {
         return deidad;
     }
 }
