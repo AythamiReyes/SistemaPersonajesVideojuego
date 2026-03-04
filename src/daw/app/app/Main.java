b39e6d0af7a745099ac9b6a1e6a4b77302cdee8f
Alejandro Acosta Arencibia <aacostaarencibia@gmail.com>
2026-03-04 19:23:41 +0000

    actualizacion del main, solo nombres para que sea algo mas realista(Lol)

--- src/daw/app/app/Main.java
+++ src/daw/app/app/Main.java
@@ -10,18 +10,22 @@ public class Main {
         JuegoController control = new JuegoController();
         ConsolaView vista = new ConsolaView();
 
-        // Asesino(nombre, nivel, salud, agilidad, sigilo)
-        Asesino ase = new Asesino("Ezio", 10, 100, 20, 80);
-        // Arquero(nombre, nivel, salud, destreza, flechas)
-        Arquero arq = new Arquero("Legolas", 8, 90, 15, 20);
-        // Clerigo(nombre, nivel, vida, mana, fuerza, int, deidad)
-        Clerigo cle = new Clerigo("Uther", 12, 150, 60, 10, 25, "Luz");
+        Asesino ase = new Asesino("Katarina", 10, 100, 20, 80);
+        Arquero arq = new Arquero("Varus", 8, 90, 15, 20);
+        Guerrero gue = new Guerrero("Garen", 9, 120, 25, 50); 
+        Clerigo cle = new Clerigo("Soraka", 12, 150, 60, 10, "Luz");
+        Mago mag = new Mago("Veigar", 15, 110, 80, 30, "Fuego");
 
         control.agregarPersonaje(ase);
         control.agregarPersonaje(arq);
+        control.agregarPersonaje(gue);
         control.agregarPersonaje(cle);
+        control.agregarPersonaje(mag);
 
         vista.mostrarEstado(control.getPersonajes());
+        
         control.simularTurno();
+        
+        vista.mostrarEstado(control.getPersonajes());
     }
 }
